42a3ddb268676144c503b407e3d1beb6186b4772
Koen Raijer <koenrayer@gmail.com>
2022-04-25 17:24:51 +0200

    rm no of posts written

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -93,7 +93,7 @@
   </section>
 
   <h2 class="text-3xl font-semibold mb-2">Latest posts</h2>
-  <h3 class="text-gray-500">I've written <div class="inline">{posts.length}</div> essays, notes, tutorials and snippets.</h3>
+  <!--<h3 class="text-gray-500">I've written <div class="inline">{posts.length}</div> essays, notes, tutorials and snippets.</h3>-->
   <div class="relative w-full my-4 mt-2">
       <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
         <svg class="w-5 h-5 text-base-content" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
