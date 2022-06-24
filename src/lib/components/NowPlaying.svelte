52dd3d674df73b1c43271ed74d84c8ca351af64c
Koen Raijer <koenrayer@gmail.com>
2022-06-24 14:11:35 +0200

    nowplaying changes

--- src/lib/components/NowPlaying.svelte
+++ src/lib/components/NowPlaying.svelte
@@ -40,21 +40,21 @@
 </script>
 
 {#key isOn}
-    <div on:mouseenter={entered} on:mouseleave={left} class="group nowPlayingComponent inline-flex px-3 py-1 rounded-md items-center bg-gray-100 transition-colors decoration-none hover:bg-gray-700 dark:hover:bg-white dark:hover:text-gray-900 hover:text-white dark:bg-gray-50/10 text-sm">
+    <div on:mouseenter={entered} on:mouseleave={left} class="group nowPlayingComponent inline-flex px-3 py-1 rounded-md items-center bg-gray-100 transition-colors decoration-none dark:bg-gray-50/10 text-sm">
         {#if !song}
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="w-5 h-5" fill="currentColor" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><circle cx="128" cy="128" r="96" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></circle><path d="M179.1,116.3a112.1,112.1,0,0,0-102.3.1" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></path><path d="M158.7,155.3A68.4,68.4,0,0,0,128,148a67.6,67.6,0,0,0-30.8,7.4" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></path></svg>
-        &nbsp;Loading - <span class="text-gray-400">&nbsp;Spotify</span>    
+        &nbsp;Loading - <span class="text-gray-400">&nbsp;Spotify</span>        
         {:else if song}
             {#if song.isPlaying === true}
-                <div class="audio relative m-0 p-0 ml-[1.5] inline-flex self-end bg-black dark:bg-white">
+                <div class="audio inline h-4 w-4 top-0">
                     <span></span>
                     <span></span>
                     <span></span>
                 </div>
-                <span class="leading-0"><span class="font-[500] ml-4"><a href={song.songUrl} rel="noopener noreferrer" target="_blank" class="hover:underline">{song.title}</a></span>&nbsp;-&nbsp;{song.artist}</span>
+                <a href={song.songUrl} rel="noopener noreferrer" target="_blank" title="{song.artist}" class="hover:underline font-[500] ml-4">{song.title}</a>
             {:else}
-                <svg class="inline h-4 w-4 top-0 -translate-y-[1.5px]" fill="currentColor" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M16 0c-8.803 0-16 7.197-16 16s7.197 16 16 16c8.803 0 16-7.197 16-16s-7.12-16-16-16zM23.36 23.12c-0.319 0.479-0.881 0.64-1.36 0.317-3.76-2.317-8.479-2.797-14.083-1.52-0.557 0.165-1.037-0.235-1.199-0.72-0.156-0.557 0.24-1.036 0.719-1.197 6.084-1.36 11.365-0.803 15.521 1.76 0.563 0.24 0.64 0.88 0.401 1.36zM25.281 18.719c-0.401 0.563-1.12 0.803-1.683 0.401-4.317-2.641-10.88-3.437-15.916-1.839-0.641 0.156-1.365-0.161-1.521-0.803-0.161-0.64 0.156-1.359 0.797-1.52 5.844-1.761 13.041-0.876 18 2.161 0.484 0.24 0.724 1.041 0.323 1.599zM25.443 14.24c-5.125-3.043-13.683-3.36-18.563-1.839-0.801 0.239-1.599-0.24-1.839-0.964-0.239-0.797 0.24-1.599 0.959-1.839 5.683-1.681 15.041-1.359 20.964 2.161 0.719 0.401 0.957 1.36 0.557 2.079-0.401 0.563-1.36 0.801-2.079 0.401z"/></svg>
-                &nbsp;Not playing - <span class="text-gray-400">&nbsp;Spotify</span>   
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="w-5 h-5" fill="currentColor" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><circle cx="128" cy="128" r="96" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></circle><path d="M179.1,116.3a112.1,112.1,0,0,0-102.3.1" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></path><path d="M158.7,155.3A68.4,68.4,0,0,0,128,148a67.6,67.6,0,0,0-30.8,7.4" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></path></svg>
+            &nbsp;Not playing - <span class="text-gray-400">&nbsp;Spotify</span>   
             {/if}
         {/if}
         <br>
@@ -63,12 +63,12 @@
 
 <style>
         .audio span {
-        width: 4px;
+        width: 3px;
         height: 100%;
         border-radius: 2px;
         position: absolute;
-        bottom: .25rem;
-        @apply bg-gray-500;
+        bottom: .45rem;
+        @apply bg-inherit dark:bg-white;
     }
     
     .audio span:first-of-type {
@@ -92,7 +92,7 @@
   
     @keyframes animationTest {
         0%   { height: 2px; }
-        50% { height: 14px; }
+        50% { height: 12px; }
         100% { height: 2px; }
     }
 
