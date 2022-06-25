f0fdd6e0249c15af3f6ed9d8f8ea59154a078cae
Koen Raijer <koenrayer@gmail.com>
2022-06-25 09:33:28 +0200

    Nowplaying fix

--- src/lib/components/NowPlaying.svelte
+++ src/lib/components/NowPlaying.svelte
@@ -40,13 +40,13 @@
 </script>
 
 {#key isOn}
-    <div on:mouseenter={entered} on:mouseleave={left} class="group nowPlayingComponent inline-flex px-3 py-1 rounded-md items-center bg-gray-100 transition-colors decoration-none dark:bg-gray-50/10 text-sm">
+    <div on:mouseenter={entered} on:mouseleave={left} class="group inline-flex px-3 py-1 rounded-md items-center bg-gray-100 transition-colors decoration-none dark:bg-gray-50/10 text-sm">
         {#if !song}
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="w-5 h-5" fill="currentColor" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><circle cx="128" cy="128" r="96" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></circle><path d="M179.1,116.3a112.1,112.1,0,0,0-102.3.1" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></path><path d="M158.7,155.3A68.4,68.4,0,0,0,128,148a67.6,67.6,0,0,0-30.8,7.4" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></path></svg>
+
         &nbsp;Loading - <span class="text-gray-400">&nbsp;Spotify</span>        
         {:else if song}
             {#if song.isPlaying === true}
-                <div class="audio inline h-4 w-4 top-0">
+                <div class="audio inline-flex h-4 w-4 relative">
                     <span></span>
                     <span></span>
                     <span></span>
@@ -67,7 +67,7 @@
         height: 100%;
         border-radius: 2px;
         position: absolute;
-        bottom: .45rem;
+        bottom: 0;
         @apply bg-slate-700 dark:bg-white;
     }
     
@@ -99,10 +99,6 @@
     @keyframes heightIncrease {
         0% { height: 3rem}
         100% { height: 100% }
-    }
-
-    .nowPlayingComponent:hover {
-        animation: heightIncrease 1.5s ease-in-out;
     }
   </style>
   
