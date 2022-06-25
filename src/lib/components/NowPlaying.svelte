4e718fa251041508d951f9541742ed7c9cc5901c
Koen Raijer <koenrayer@gmail.com>
2022-06-25 09:33:51 +0200

    np fix

--- src/lib/components/NowPlaying.svelte
+++ src/lib/components/NowPlaying.svelte
@@ -42,7 +42,7 @@
 {#key isOn}
     <div on:mouseenter={entered} on:mouseleave={left} class="group inline-flex px-3 py-1 rounded-md items-center bg-gray-100 transition-colors decoration-none dark:bg-gray-50/10 text-sm">
         {#if !song}
-
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="w-5 h-5" fill="currentColor" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><circle cx="128" cy="128" r="96" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></circle><path d="M179.1,116.3a112.1,112.1,0,0,0-102.3.1" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></path><path d="M158.7,155.3A68.4,68.4,0,0,0,128,148a67.6,67.6,0,0,0-30.8,7.4" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></path></svg>
         &nbsp;Loading - <span class="text-gray-400">&nbsp;Spotify</span>        
         {:else if song}
             {#if song.isPlaying === true}
